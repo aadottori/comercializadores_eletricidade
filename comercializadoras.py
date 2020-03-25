e001e7143873078ea1c44980bda0e9d666b97440
Antonio Dottori <37637976+aadottori@users.noreply.github.com>
2020-03-25 02:25:37 -0300

    Add files via upload

--- comercializadoras.py
+++ comercializadoras.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from trataDados import *
 
 def comercio():
     firefox = webdriver.Firefox()
@@ -14,6 +13,36 @@ def comercio():
         firefox.find_element_by_xpath('/html/body/form/center/table/tbody/tr[2]/td/select/option['+str(id)+']').click()
         firefox.find_element_by_xpath(path_botao_pesquisar).click()
         time.sleep(1)
+
+        path_empresa = '/html/body/form/center/center/table/tbody/tr[1]/td'
+        path_endereco = '/html/body/form/center/center/table/tbody/tr[2]/td'
+        path_TelefoneFaxEmail = '/html/body/form/center/center/table/tbody/tr[3]/td[1]'
+        path_sede = '/html/body/form/center/center/table/tbody/tr[3]/td[2]'
+        path_representantes = '/html/body/form/center/center/table/tbody/tr[4]/td'
+        path_CgcCnpj = '/html/body/form/center/center/table/tbody/tr[5]/td[1]'
+        path_registroMAE = '/html/body/form/center/center/table/tbody/tr[5]/td[2]'
+        path_processo = '/html/body/form/center/center/table/tbody/tr[6]/td'
+
+        empresa = firefox.find_element_by_xpath(path_empresa).text
+        endereco = firefox.find_element_by_xpath(path_endereco).text
+        telefone_fax_email = firefox.find_element_by_xpath(path_TelefoneFaxEmail).text
+        sede = firefox.find_element_by_xpath(path_sede).text
+        representantes = firefox.find_element_by_xpath(path_representantes).text
+        cgccnpj = firefox.find_element_by_xpath(path_CgcCnpj).text
+        registroMAE = firefox.find_element_by_xpath(path_registroMAE).text
+        processo = firefox.find_element_by_xpath(path_processo).text
+
+        print (empresa)
+        print (endereco)
+        print (telefone_fax_email)
+        print (sede)
+        print (representantes)
+        print (cgccnpj)
+        print (registroMAE)
+        print (processo)
+        print ('-------')
+
+
 
 if __name__ == "__main__":
     comercio()
